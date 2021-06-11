e310e9118d484244458519939827703d65cc171c
Michael Ahlers <michael@ahlers.consulting>
2021-06-11 16:56:32 -0400

    First attempt at configuring stats receiver

--- src/main/scala/ahlers/learn/twitter/MessageHttpServer.scala
+++ src/main/scala/ahlers/learn/twitter/MessageHttpServer.scala
@@ -1,8 +1,11 @@
 package ahlers.learn.twitter
 
-import scala.reflect.runtime.universe._
+import com.google.inject
+import com.twitter.finagle.Http
 
+import scala.reflect.runtime.universe._
 import com.twitter.finagle.http.{ Request, Response }
+import com.twitter.finagle.stats.{ NullStatsReceiver, StatsReceiver }
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.{ CommonFilters, LoggingMDCFilter, TraceIdMDCFilter }
 import com.twitter.finatra.http.routing.HttpRouter
@@ -15,6 +18,12 @@ import com.twitter.inject.Logging
 class MessageHttpServer
   extends HttpServer
     with Logging {
+
+  override protected def statsReceiverModule =
+    MessageStatsReceiverModule
+
+  override protected def start(): Unit =
+    injector.instance[StatsReceiver]
 
   override protected def warmup() =
     handle[MessageWarmupHandler]()
